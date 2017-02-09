94a24970f30827466b3326eabf711d83e8fbd8e6
evgen.solodilov <evgen.solodilov@gmail.com>
2017-02-09 13:31:21 +0200

    refactoring to fragment

---
+++ app/src/main/java/ua/dp/oleg/maliy/lexicoach/FirstFragment.java
@@ -0,0 +1,92 @@
+package ua.dp.oleg.maliy.lexicoach;
+
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class FirstFragment extends Fragment {
+    @BindView(R.id.buttonAdd)
+    protected Button buttonAdd;
+    @BindView(R.id.buttonOwn)
+    protected Button buttonOwn;
+    @BindView(R.id.buttonBell)
+    protected ImageButton buttonBell;
+    @BindView(R.id.buttonBox)
+    protected ImageButton buttonBox;
+    @BindView(R.id.englishWord)
+    protected CustomTextView englishWord;
+    @BindView(R.id.hebrewWordZero)
+    protected CustomTextView hebrewWordZero;
+    @BindView(R.id.hebrewWordOne)
+    protected CustomTextView hebrewWordOne;
+    @BindView(R.id.hebrewWordTwo)
+    protected CustomTextView hebrewWordTwo;
+    @BindView(R.id.hebrewWordThree)
+    protected CustomTextView hebrewWordThree;
+    public FirstFragment() {
+    }
+
+    public static FirstFragment newInstance() {
+        FirstFragment fragment = new FirstFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_first, container, false);
+        ButterKnife.bind(this,rootView);
+        return rootView;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        englishWord.setText(getString(R.string.englishWordOne));
+
+        hebrewWordZero.setText(getString(R.string.hebrewWordZero));
+        hebrewWordOne.setText(getString(R.string.hebrewWordOne));
+        hebrewWordTwo.setText(getString(R.string.hebrewWordTwo));
+        hebrewWordThree.setText(getString(R.string.hebrewWordThree));
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+    }
+
+    @OnClick(R.id.buttonAdd)
+    void buttonAdd() {
+        Toast.makeText(getActivity(), getString(R.string.button_add), Toast.LENGTH_SHORT).show();
+    }
+
+    @OnClick(R.id.buttonOwn)
+    void buttonOwn() {
+        Toast.makeText(getActivity(), getString(R.string.button_own), Toast.LENGTH_SHORT).show();
+    }
+
+    @OnClick(R.id.buttonBell)
+    void buttonBell() {
+        Toast.makeText(getActivity(), getString(R.string.button_bell), Toast.LENGTH_SHORT).show();
+    }
+
+    @OnClick(R.id.buttonBox)
+    void buttonBox() {
+        Toast.makeText(getActivity(), getString(R.string.button_box), Toast.LENGTH_SHORT).show();
+    }
+}
--- app/src/main/java/ua/dp/oleg/maliy/lexicoach/MainActivity.java
+++ app/src/main/java/ua/dp/oleg/maliy/lexicoach/MainActivity.java
@@ -1,5 +1,6 @@
 package ua.dp.oleg.maliy.lexicoach;
 
+import android.app.FragmentManager;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
@@ -10,86 +11,41 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.Toast;
 
-import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity {
 
+    private FragmentManager mFragmentManager;
     private DrawerLayout drawerLayout;
-    private View content;
     private Toolbar toolbar;
     private NavigationView navigationView;
     private ActionBarDrawerToggle drawerToggle;
 
-    @BindView(R.id.buttonAdd)
-    protected Button buttonAdd;
-    @BindView(R.id.buttonOwn)
-    protected Button buttonOwn;
-    @BindView(R.id.buttonBell)
-    protected ImageButton buttonBell;
-    @BindView(R.id.buttonBox)
-    protected ImageButton buttonBox;
-    @BindView(R.id.englishWord)
-    protected CustomTextView englishWord;
-    @BindView(R.id.hebrewWordZero)
-    protected CustomTextView hebrewWordZero;
-    @BindView(R.id.hebrewWordOne)
-    protected CustomTextView hebrewWordOne;
-    @BindView(R.id.hebrewWordTwo)
-    protected CustomTextView hebrewWordTwo;
-    @BindView(R.id.hebrewWordThree)
-    protected CustomTextView hebrewWordThree;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mFragmentManager = getFragmentManager();
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         initToolbar();
         setupDrawerLayout();
-
         ButterKnife.bind(this);
 
-        content = findViewById(R.id.content);
         drawerToggle = setupDrawerToggle();
-
-        englishWord.setText(getString(R.string.englishWordOne));
-
-        hebrewWordZero.setText(getString(R.string.hebrewWordZero));
-        hebrewWordOne.setText(getString(R.string.hebrewWordOne));
-        hebrewWordTwo.setText(getString(R.string.hebrewWordTwo));
-        hebrewWordThree.setText(getString(R.string.hebrewWordThree));
-
-//        final ImageView avatar = (ImageView) navigationView.getHeaderView(0).findViewById(R.id.avatar);
-//        Picasso.with(this).load(Const.AVATAR_URL).transform(new CircleTransform()).into(avatar);
-
+        if (savedInstanceState == null) {
+            startIntroFragment();
+        }
     }
 
-    @OnClick(R.id.buttonAdd)
-    void buttonAdd() {
-        Toast.makeText(MainActivity.this, getString(R.string.button_add), Toast.LENGTH_SHORT).show();
-    }
-
-    @OnClick(R.id.buttonOwn)
-    void buttonOwn() {
-        Toast.makeText(MainActivity.this, getString(R.string.button_own), Toast.LENGTH_SHORT).show();
-    }
-
-    @OnClick(R.id.buttonBell)
-    void buttonBell() {
-        Toast.makeText(MainActivity.this, getString(R.string.button_bell), Toast.LENGTH_SHORT).show();
-    }
-
-    @OnClick(R.id.buttonBox)
-    void buttonBox() {
-        Toast.makeText(MainActivity.this, getString(R.string.button_box), Toast.LENGTH_SHORT).show();
+    private void startIntroFragment() {
+        mFragmentManager
+                .beginTransaction()
+                .add(R.id.activity_container, FirstFragment.newInstance())
+                .commit();
     }
 
     private void initToolbar() {
